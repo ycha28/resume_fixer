89e9aea839c1306cbc2fa39a1a9fab343409ff95
Young <young.cha@hyfn.com>
2015-05-10 17:04:28 -0700

    fixed login flash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :sanitize_flash_params, if: :flash_keys_present?
 
   def after_sign_in_path_for(resource)
     profile_documents_path
@@ -12,5 +13,16 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << [:name, :phone_number]
+  end
+
+  def flash_keys_present?
+    flash.keys.any?
+  end
+
+  def sanitize_flash_params
+    if flash[:notice].present?
+      flash[:success] = flash[:notice]
+      flash.discard(:notice)
+    end
   end
 end
