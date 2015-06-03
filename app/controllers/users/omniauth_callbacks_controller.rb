233a83f32e3501aef22f4725f0bb7aae4e0d0120
Young <young.cha@hyfn.com>
2015-06-02 21:56:45 -0700

    skip confirmation for fb users

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,6 +6,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     if @user.persisted?
       remember_me(@user)
+      @user.skip_confirmation!
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
